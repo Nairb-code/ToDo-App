cccee112c8d1e57c136973b414cb008adbc400ae
Brian Duran <brian.ez.var@gmail.com>
2023-04-08 15:36:17 -0300

    Login realizado. [Version1]

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
+   const email = document.querySelector("#inputEmail")
+   const password = document.querySelector("#inputPassword")
+   const form = document.forms[0]
+   const url = 'https://todo-api.ctd.academy/v1'
     
 
 
@@ -9,11 +12,23 @@ window.addEventListener('load', function () {
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-       
-        
+    
+        event.preventDefault()
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+    
+    const settings = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
 
-
-
+    realizarLogin(settings)
+    form.reset()
     });
 
 
@@ -21,8 +36,28 @@ window.addEventListener('load', function () {
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarLogin(settings) {
-       
 
+    fetch(`${url}/users/login`, settings)
+        .then( response => {
+            console.log("Promesa complida: ", response)
+            if(response.ok != true){
+                alert("Algunos datos son incorrectos.")
+            }
+            return response.json()
+        })
+        .then( data => {
+
+            console.log(data)
+            if(data.jwt){
+                localStorage.setItem("JWT", JSON.stringify(data.jwt))
+            }
+
+            location.replace("./mis-tareas.html")
+        })
+        .catch( err => {
+            alert("Promesa rechazada")
+            console.log(err)
+        })
 
 
 
